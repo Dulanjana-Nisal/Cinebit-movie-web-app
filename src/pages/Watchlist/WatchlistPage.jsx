8c971cc557c9b1cbe3a1ea33aa4072a3d7ff457e
dulanjananisal <dulanjananisal67@gmail.com>
2026-03-17 00:22:20 +0530

    update styles

--- src/pages/Watchlist/WatchlistPage.jsx
+++ src/pages/Watchlist/WatchlistPage.jsx
@@ -62,47 +62,50 @@ function WatchlistPage() {
                                     <p>Loading...</p>
                                 </div> :
 
-                                localstorageData && localstorageData.length > 0 ? 
-                                localstorageData.map((data) => {
+                                localstorageData && localstorageData.length > 0 ?
+                                    localstorageData.map((data) => {
 
-                                    let alredyList = localstorageData.length > 0 && localstorageData.find((findData) => { return findData.id === data.id })
+                                        let alredyList = localstorageData.length > 0 && localstorageData.find((findData) => { return findData.id === data.id })
 
-                                    return (
-                                        <div className="movie-card" key={data.id}> 
-                                            <div className="card-head">
-                                                <Link to={`/${data.type}/${data.id}`}>
-                                                    {!data.poster_path ? <img src={imageNotFound} /> : <img src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} />}
-                                                </Link>
-                                                <div className={data.type === 'movie' ? "tag-movie" : "tag-tv"}><p>{data.type === 'movie' ? 'Movie' : 'TV'}</p></div>
-                                                <div className="card-functions">
-                                                    <dev className={alredyList ? 'fav fav-add' : 'fav'} onClick={() =>{removeWatchlist(data.id) }} >
-                                                        <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                                                    </dev>
+                                        return (
+                                            <div className="movie-card watchlist-card" key={data.id}>
+                                                <div className="card-head">
                                                     <Link to={`/${data.type}/${data.id}`}>
-                                                        <div className="play">
-                                                            <svg width="26" height="26" fill="#fff" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"></path></svg>
-                                                        </div>
+                                                        {!data.poster_path ? <img src={imageNotFound} /> : <img src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} />}
                                                     </Link>
-                                                </div>
-                                            </div>
-                                            <Link to={`/${data.type}/${data}`}>
-                                                <div className="card-body">
-                                                    <div className="title">
-                                                        <p>{data.title ? data.title : data.name}</p>
+                                                    <div className={data.type === 'movie' ? "tag-movie" : "tag-tv"}><p>{data.type === 'movie' ? 'Movie' : 'TV'}</p></div>
+                                                    <div className="card-functions">
+                                                        <dev className={alredyList ? 'fav fav-add' : 'fav'} onClick={() => { removeWatchlist(data.id) }} >
+                                                            <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                                                        </dev>
+                                                        <Link to={`/${data.type}/${data.id}`}>
+                                                            <div className="play">
+                                                                <svg width="26" height="26" fill="#fff" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"></path></svg>
+                                                            </div>
+                                                        </Link>
                                                     </div>
                                                 </div>
-                                            </Link>
-                                        </div>
-                                    )
-                                })
-                                :
-                                <div className="empty-watchlist">
-                                    <svg width="40" height="40" fill="none" stroke="#f87171" viewBox="0 0 24 24" stroke-width="1.5"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                                    <h1>Your Watchlist is Empty</h1>
-                                    <p>Start adding movies and TV Shows you want to watch later</p>
-                                    <Link to='/movies'><button>Browse Content</button></Link>
-                                </div>
-                                }
+                                                <Link to={`/${data.type}/${data}`}>
+                                                    <div className="card-body">
+                                                        <div className="title">
+                                                            <p>{data.title ? data.title : data.name}</p>
+                                                        </div>
+                                                        <div className="data">
+                                                            <div className="ratings"><svg width="14" height="14" fill="currentColor" viewBox="0 0 24 24"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> {Number(data.vote_average).toFixed(1)}</div>
+                                                        </div>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        )
+                                    })
+                                    :
+                                    <div className="empty-watchlist">
+                                        <svg width="40" height="40" fill="none" stroke="#f87171" viewBox="0 0 24 24" stroke-width="1.5"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                                        <h1>Your Watchlist is Empty</h1>
+                                        <p>Start adding movies and TV Shows you want to watch later</p>
+                                        <Link to='/movies'><button>Browse Content</button></Link>
+                                    </div>
+                        }
                     </div>
                 </div>
             </div>
